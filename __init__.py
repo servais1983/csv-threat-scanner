cc071d704bc07d4c147177b9ea5048943e780b87
servais1983 <stservais0409@gmail.com>
2025-04-04 21:36:09 +0200

    Ajout du fichier d'initialisation du package

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""
+CSV Threat Scanner - Un outil pour analyser les fichiers CSV à la recherche de comportements suspects
+et de menaces potentielles dans votre environnement informatique.
+"""
+
+__version__ = '0.1.0'
